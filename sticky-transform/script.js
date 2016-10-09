1a9db2e824c3cb248868fe584b43ffd4f0aadad7
Christopher Campbell <christophercam@gmail.com>
2016-10-09 14:00:41 +0200

    add sticky transform

--- sticky-transform/script.js
+++ sticky-transform/script.js
@@ -1,14 +1,19 @@
 var scrollPosition;
+var ticking = false;
 var secondSlide = document.querySelector('.slide.second');
 
 window.addEventListener('scroll', onScroll);
 
 function update() {
-    var elementPos = secondSlide.scrollHeight - window.pageYOffset;
+    var elementPos = secondSlide.offsetTop - window.pageYOffset;
     var scrollPos = elementPos > 0 ? 0 : elementPos * (-1);
     secondSlide.style.transform = 'translateY(' + scrollPos + 'px)';
+    ticking = false;
 }
 
 function onScroll() {
-    window.requestAnimationFrame(update);
+    if (!ticking) {
+        window.requestAnimationFrame(update);
+    }
+    ticking = true;
 }
